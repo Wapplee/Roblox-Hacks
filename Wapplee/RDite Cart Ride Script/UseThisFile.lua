1f23e9064b8ef857d248b2cba69ba1335d0ec1af
Wapplee <83174774+Wapplee@users.noreply.github.com>
2021-12-08 00:45:40 -0500

    Create UseThisFile.lua

---
+++ Wapplee/RDite Cart Ride Script/UseThisFile.lua
@@ -0,0 +1,7 @@
+-- SETTINGS
+_G.typeButton = "Down" -- Up, Down or On
+_G.randomButtons = false
+_G.loop = true
+_G.cartAlwaysStatus = true -- true = on, false = off, nil = choosable
+-- SETTINGS
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wapplee/Roblox-Hacks/main/Wapplee/RDite%20Cart%20Ride%20Script/main.lua"))()
