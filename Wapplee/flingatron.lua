6fb3c387b175fc7bf126eab93f1ecbb20be90c20
Wapplee <83174774+Wapplee@users.noreply.github.com>
2021-12-09 23:42:28 -0500

    Create flingatron.lua

---
+++ Wapplee/flingatron.lua
@@ -0,0 +1,79 @@
+-- loadstring(game:HttpGet'')()
+local gayKeybinds = false
+local flingSpeed = 100
+
+
+
+local cps = 0
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local tool = Instance.new("Tool",LocalPlayer.Backpack)
+tool.RequiresHandle = false
+local equip = false
+local mouse = LocalPlayer:GetMouse()
+tool.Activated:Connect(function()
+    task.delay(.5,function()
+        cps = cps-1.5
+    end)
+    cps = cps+1.5
+    local cha= LocalPlayer.Character
+    if cha then
+        local e = cha:FindFirstChild("HumanoidRootPart")
+        if e then
+            local mouseHit = mouse.Hit
+            e.Velocity = CFrame.new(e.Position,mouseHit.Position).LookVector*flingSpeed*(1+(cps/2))
+        end
+    end
+end)
+mouse.KeyDown:Connect(function(c)
+    c=c:lower()
+    if equip and c == "'" then
+        gayKeybinds = not gayKeybinds
+    end
+    if gayKeybinds == false then return end
+    if equip and c == "q" then
+        local cha= LocalPlayer.Character
+        if cha then
+            local e = cha:FindFirstChild("HumanoidRootPart")
+            if e then
+                local mouseHit = mouse.Hit
+                e.Velocity = CFrame.new(0,0,0).LookVector*0
+            end
+        end
+    end
+    if equip and c == "e" then
+        local cha= LocalPlayer.Character
+        if cha then
+            local e = cha:FindFirstChild("HumanoidRootPart")
+            if e then
+                 local mouseHit = mouse.Hit
+                e.Velocity = CFrame.new(e.Position,mouseHit.Position).LookVector*500
+            end
+        end
+    end
+    if equip and c == "r" then
+        local cha= LocalPlayer.Character
+        if cha then
+            local e = cha:FindFirstChild("HumanoidRootPart")
+            if e then
+                cha:SetPrimaryPartCFrame(cha.PrimaryPart.CFrame*CFrame.new(0,0,-5))
+            end
+        end
+    end
+    if equip and c == "t" then
+        local cha= LocalPlayer.Character
+        if cha then
+            local e = cha:FindFirstChild("HumanoidRootPart")
+            if e then
+                cha:SetPrimaryPartCFrame(mouse.Hit*CFrame.new(0,5,0))
+            end
+        end
+    end
+end)
+tool.Equipped:Connect(function()
+    equip = true
+end)
+tool.Unequipped:Connect(function()
+    equip = false
+end)
+    
+tool.Name = "flingatron"
