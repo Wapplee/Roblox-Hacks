b9e751ec6a088281e3c62dd44f62ff524c5a2153
Wapplee <83174774+Wapplee@users.noreply.github.com>
2021-12-12 19:03:38 -0500

    Update UIHelper.lua

--- Modules/UIHelper.lua
+++ Modules/UIHelper.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet(""))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Wapplee/Roblox-Hacks/main/Modules/UIHelper.lua"))()
 
 local function dragfunction(title,main)
 	spawn(function()
