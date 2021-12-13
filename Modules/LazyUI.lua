1dea30bcf05de6de0750f776344a706d397b62aa
Wapplee <83174774+Wapplee@users.noreply.github.com>
2021-12-13 15:44:28 -0500

    Update LazyUI.lua

--- Modules/LazyUI.lua
+++ Modules/LazyUI.lua
@@ -239,6 +239,7 @@ function module.new(TYPE,prop)
 		Frame4.BackgroundColor3 = Color3.new(0.243137, 0.243137, 0.243137)
 		Frame4.BorderSizePixel = 0
 		Frame4.AutomaticSize = "Y"
+		Frame4.ClipsDescendants = false
 		
 		TextButton3.Name = "Toggle"
 		TextButton3.Parent = Frame1
