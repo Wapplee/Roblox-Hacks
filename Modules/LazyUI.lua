8674adc85f688ed3aaae3e4694db44f7b8c3c655
Wapplee <83174774+Wapplee@users.noreply.github.com>
2021-12-22 01:30:39 -0500

    Update LazyUI.lua

--- Modules/LazyUI.lua
+++ Modules/LazyUI.lua
@@ -5,28 +5,30 @@ local n = Instance.new
 
 local function dragfunction(title,main)
 	spawn(function()
-		main = main or title
-		local connection
-		title.InputBegan:Connect(function(old_inp)
-			if old_inp.UserInputType == Enum.UserInputType.MouseButton1 then
-				local start_pos = main.Position
-				connection = game:GetService("UserInputService").InputChanged:Connect(function(new_inp)
-					if new_inp.UserInputType == Enum.UserInputType.MouseMovement then
-						local delta = new_inp.Position-old_inp.Position
-						main.Position = UDim2.new(
-							start_pos.X.Scale, 
-							start_pos.X.Offset + delta.X, 
-							start_pos.Y.Scale, 
-							start_pos.Y.Offset + delta.Y
-						)
-					end
-				end)
-			end
-		end)
-		title.InputEnded:Connect(function(cur_input)
-			if cur_input.UserInputType == Enum.UserInputType.MouseButton1 then
-				connection:Disconnect()
-			end
+		pcall(function()
+			main = main or title
+			local connection
+			title.InputBegan:Connect(function(old_inp)
+				if old_inp.UserInputType == Enum.UserInputType.MouseButton1 then
+					local start_pos = main.Position
+					connection = game:GetService("UserInputService").InputChanged:Connect(function(new_inp)
+						if new_inp.UserInputType == Enum.UserInputType.MouseMovement then
+							local delta = new_inp.Position-old_inp.Position
+							main.Position = UDim2.new(
+								start_pos.X.Scale, 
+								start_pos.X.Offset + delta.X, 
+								start_pos.Y.Scale, 
+								start_pos.Y.Offset + delta.Y
+							)
+						end
+					end)
+				end
+			end)
+			title.InputEnded:Connect(function(cur_input)
+				if cur_input.UserInputType == Enum.UserInputType.MouseButton1 then
+					connection:Disconnect()
+				end
+			end)
 		end)
 	end)
 end
